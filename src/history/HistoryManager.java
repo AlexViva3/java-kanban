792d2e65d8868ef38a127832423607f7487abe66
User AlexViva <alexandrvavs@gmail.com>
2025-01-30 21:11:42 +0300

    ТЗ 6 Добавил в ветку класс Node и TaskCycler, сделал первый код по двухсвязному списку и истории тасков :(

--- src/history/HistoryManager.java
+++ src/history/HistoryManager.java
@@ -1,11 +1,8 @@
 package history;
 import datapacks.Task;
 import java.util.List;
-
 public interface HistoryManager {
     void addTaskHistory(Task task);
-
     void remove(int id);
-
     List<Task> getHistory();
 }
